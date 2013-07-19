893d0c0e69f2d50ab58a8e6e638e69405c6b7417
Karim Zahed <karim.zahed@gmail.com>
2013-07-19 17:52:55 +0200

    Created Generate points, and calculated distance

--- main.m
+++ main.m
@@ -1,16 +1,29 @@
 tic 
 
-UE=zeros(42,3); % Preallocating 42 UEs ( 6 in the cell and others are in the adjacent 6 cells)
+Center= [0, 0];% Center of the Central Cell
+
+r=5; %length of hexagon's side
+UE= GeneratePoints(r,Center); % List of UEs 
 
 
-r=5; %length of hexagon's side !
-UE= GeneratePoints(r);
+Base1=[Center(1)+r/2,Center(2)+sqrt(3)*r];  % Base1 Location
+% Base2=[Center(1)+r/2,Center(2)+sqrt(3)*r];  % Base1 Location
+% Base3=[Center(1)+r/2,Center(2)+sqrt(3)*r];  % Base1 Location
+% Base4=[Center(1)+r/2,Center(2)+sqrt(3)*r];  % Base1 Location
+
+
+UE(1:18,3)=( ( UE(1:18,1)-Base1(1) ).^2 +(UE(1:18,2)-Base1(2) ).^2).^(1/2); % Set 1,2,3 follow Base1
+
+
+
 
 hold off
 plot(UE(:,1),UE(:,2),'o')
 hold on 
-plot(4*r,3*sqrt(3)*r,'rx')
-axis([4 36 4 50])
+plot(Center(1),Center(2),'rx', Base1(1),Base1(2),'k+')
+
+axis([-20 20 -25 25])
+
 
 %For distribution of Field Strength
 % E_sigma=5.5; 
