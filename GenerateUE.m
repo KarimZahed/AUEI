aa46b0c0ddc9ba06e37f339fee42c731c1192b21
Karim Zahed <karim.zahed@gmail.com>
2013-07-22 17:10:04 +0200

    calculation of angle, still needs work

--- GenerateUE.m
+++ GenerateUE.m
@@ -3,7 +3,7 @@ function [ UE ] = GenerateUE(r,Centers,Base)
 
 std=r*sqrt(3)/(2*3);
 
-UE=zeros(42,4); % 3rd would hold distance, 4th column would hold E value
+UE=zeros(42,5); % 3rd would hold distance,4th would hold angle with station, 5th column would hold E value
 RAND=std.*randn(42,2);
 
 % X coordinates
@@ -25,7 +25,7 @@ UE(31:36,2)=Centers(1,2)+-sqrt(3)*r/2+RAND(31:36,2);%(6)
 UE(37:42,2)=Centers(1,2)+sqrt(3)*r/2+RAND(37:42,2); %(7)
 
 
-% Calculation of Distance of UEs to respective Base Station
+% Distance between UEs and respective Base Station
 UE(1:18,3)=( ( UE(1:18,1)-Base(1,1) ).^2 +(UE(1:18,2)-Base(1,2) ).^2).^(1/2); % Set 1,2,3 follow Base1
 UE(19:24,3)=( ( UE(19:24,1)-Base(2,1) ).^2 +(UE(19:24,2)-Base(2,2) ).^2).^(1/2); % Set 4 follow Base 2
 UE(25:36,3)=( ( UE(25:36,1)-Base(3,1) ).^2 +(UE(25:36,2)-Base(3,2) ).^2).^(1/2); % Set 5,6 follow Base 3
--- main.m
+++ main.m
@@ -4,17 +4,7 @@ r=5; %length of hexagon's side
 
 %%
 [Centers,Borders,Base]=Initiate(r);
-
-%% Generation of UE will be recurring
-% Generate the UE location, and calculates Distance to respective Base
-UE= GenerateUE(r,Centers,Base); % List of UEs 
-% Make sure of the variables
-% Calculate E for each of the 42 UEs in consideration
-% E=E_predict('land',600,z,50,600,600,600,10,0,10,'suburban',0,0,0,1); %
-
-
-
-% DTT=zeros(10000,0);
+%% DTT=zeros(10000,0);
 % distance here is calculated once between DTT pixel center and BS, then E is calculated once
 %gaussian distribution is then applied 
 %For distribution of Field Strength
@@ -24,8 +14,33 @@ UE= GenerateUE(r,Centers,Base); % List of UEs
 % Gauss=1/(sqrt(2*pi)*E_sigma)*exp(-0.5*(X-E_med).^2/(E_sigma^2));% Creates
 % the Gaussian function for DTT distribution
 
+%% Generation of UE will be recurring
+% Generate the UE location, and calculates Distance to respective Base
+UE= GenerateUE(r,Centers,Base); % List of UEs 
+% Make sure of the variables
+% Calculate E for each of the 42 UEs in consideration
+% E=E_predict('land',600,z,50,600,600,600,10,0,10,'suburban',0,0,0,1); %
 
+%%
+a=UE(1:6,1) -Base(1,1);
+b=UE(1:6,2)-Base(1,2) ;
+UEv= [a ,b ];
+normU=( ( UEv(1:6,1)).^2 +(UEv(1:6,2).^2)).^(1/2);
 
+Ref1=zeros(6,2);
+Ref1(1:6,1)=Centers(1,1)-Base(1,1);
+Ref1(1:6,2)=Centers(1,2)-Base(1,2);
+norm1=( ( Ref1(1:6,1)).^2 +(Ref1(1:6,2).^2)).^(1/2);
+
+dot=UEv(1:6,1).*Ref1(1:6,1)+UEv(1:6,2).*Ref1(1:6,2);
+
+UE(1:6,4)= acos(dot./(normU.*norm1));
+ 
+% UEv= [UE(1,1) , UE(1,2)] - [Base(1,1),Base(1,2)];
+% Ref1=[Centers(1,1) , Centers(1,2)] - [Base(1,1),Base(1,2)];
+% UE(1,4)= acos (dot(UEv,Ref1)/(norm(UEv)*norm(Ref1)))
+
+%%
 hold off % Plots UE randomly distributed pointspoints
 plot(UE(:,1),UE(:,2),'o')
 
